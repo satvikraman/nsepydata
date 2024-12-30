864bfe61f9be357f69a81e6aeda90c2ee4690b00
Satvik Raman <satvikraman11@gmail.com>
2024-12-30 23:37:24 +0530

    + Checking in a sample application code to test the library

--- examples/appnse.py
+++ examples/appnse.py
@@ -12,12 +12,5 @@ symbol = 'NESTLEIND'
 
 os.makedirs(folder, exist_ok=True)
 
-nse_df, nse_corp_act_df = histApp.download_historical_data(symbol, 'EQ', '01-Jan-2023', '12-Dec-2024', True)
-nse_df.to_csv(folder+symbol+'_raw.csv', index=False)
-nse_corp_act_df.to_csv(folder+symbol+'_corp_action.csv', index=False)
-
-nse_df = pd.read_csv(folder+symbol+'_raw.csv')
-nse_corp_act_df = pd.read_csv(folder+symbol+'_corp_action.csv')
-nse_df = histApp.getOHLCV(nse_df)
-nse_df = histApp.adjust_for_corp_action(nse_df, nse_corp_act_df)
-nse_df.to_csv(folder+symbol+'.csv', index=False)
+nse_df, nse_corp_act_df = histApp.get_OHLCV_data(symbol=symbol, series='EQ', start='01-Feb-2023', end='12-Dec-2024', adjust_corp_action=True, timeperiod='2M')
+nse_df.to_csv(folder+symbol+'_2M.csv', index=False)
