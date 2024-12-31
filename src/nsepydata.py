3d2415bd75b215697d6c28c4cfae1938160c6dfc
Satvik Raman <satvikraman11@gmail.com>
2024-12-31 19:51:46 +0530

    + Closes #3 The class now downloads the stocks available in the Equity and SME segment on initialization. This is then used to fetch the series value automatically.

--- src/nsepydata.py
+++ src/nsepydata.py
@@ -31,6 +31,43 @@ class NSEPyData():
             "Sec-Fetch-Site": "none",
             "Sec-Fetch-User": "?1",
         }
+        self.equity_df = self.get_securities_in_segment('EQUITY')
+        self.sme_df = self.get_securities_in_segment('SME')
+
+
+    def __download_csv(self, url, host=None, params=None):
+        # Initialize the final DataFrame
+        nse_df = pd.DataFrame()
+
+        # Session to manage cookies automatically
+        with requests.Session() as session:
+            headers = {
+                        "Host": "www.nseindia.com",
+                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0",
+                        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                        "Accept-Language": "en-US,en;q=0.5",
+                        "Accept-Encoding": "gzip, deflate, br, zstd",
+                        "Connection": "keep-alive",
+                        "Upgrade-Insecure-Requests": "1",
+                        "Sec-Fetch-Dest": "document",
+                        "Sec-Fetch-Mode": "navigate",
+                        "Sec-Fetch-Site": "none",
+                        "Sec-Fetch-User": "?1",
+                    }
+            if host is not None:
+                headers['Host'] = host
+            # Send an initial request to the main site to get cookies and headers
+            session.get('https://'+host, headers=headers)
+            # Make the actual request to download the CSV file
+            response = session.get(url, headers=headers, params=params)
+            
+            # If response is not empty, read the csv file
+            if response.content.strip():
+                try:
+                    nse_df = pd.read_csv(StringIO(response.text))
+                except Exception as e:
+                    self.__logger.error(f"Error parsing response: {e}")
+        return nse_df  
 
 
     def __download_historical_price_volume(self, symbol, series, start=None, end=None):
@@ -94,10 +131,6 @@ class NSEPyData():
         return nse_df           
 
 
-    def __download_all_corporate_actions(self):
-        url = "https://www.nseindia.com/api/corporates-corporateActions?index=equities&from_date=05-01-2024&to_date=05-12-2024&csv=true"
-
-
     def __get_action_factor(self, purpose):
         actions = {"split": "SPLIT", "bonus": "BONUS"}
         action = None
@@ -122,19 +155,20 @@ class NSEPyData():
         return action, factor
 
 
-    def __adjust_for_corp_action(self, ohlcv_df, corp_act_df):    
-        # Filter rows containing the words "Split" or "Bonus"
-        filt_corp_act_df = corp_act_df[corp_act_df['PURPOSE'].str.contains('Split|Bonus', case=False, na=False)]
-        start = None
-        adj_columns = ['OPEN', 'HIGH', 'LOW', 'CLOSE']
-        for index, row in filt_corp_act_df.iterrows():
-            record_date = row['RECORD DATE']
-            purpose = row['PURPOSE']
-            action, factor = self.__get_action_factor(purpose)
-            if action is not None:
-                start = datetime.strptime(record_date, '%d-%b-%Y')
-                ohlcv_df.loc[(ohlcv_df['DATE'] < start), adj_columns] = (ohlcv_df[adj_columns] / factor).round(2)
-                ohlcv_df.loc[(ohlcv_df['DATE'] < start), 'VOLUME'] = (ohlcv_df['VOLUME'] * factor).astype(int)
+    def __adjust_for_corp_action(self, ohlcv_df, corp_act_df):   
+        if not corp_act_df.empty:
+            # Filter rows containing the words "Split" or "Bonus"
+            filt_corp_act_df = corp_act_df[corp_act_df['PURPOSE'].str.contains('Split|Bonus', case=False, na=False)]
+            start = None
+            adj_columns = ['OPEN', 'HIGH', 'LOW', 'CLOSE']
+            for index, row in filt_corp_act_df.iterrows():
+                record_date = row['RECORD DATE']
+                purpose = row['PURPOSE']
+                action, factor = self.__get_action_factor(purpose)
+                if action is not None:
+                    start = datetime.strptime(record_date, '%d-%b-%Y')
+                    ohlcv_df.loc[(ohlcv_df['DATE'] < start), adj_columns] = (ohlcv_df[adj_columns] / factor).round(2)
+                    ohlcv_df.loc[(ohlcv_df['DATE'] < start), 'VOLUME'] = (ohlcv_df['VOLUME'] * factor).astype(int)
 
         return ohlcv_df
 
@@ -142,8 +176,7 @@ class NSEPyData():
     def __extractOHLCV(self, nse_df):
         columns = ["DATE", "OPEN", "HIGH", "LOW", "CLOSE", "VOLUME"]
         OHLCV_df = nse_df[columns]
-        OHLCV_df = OHLCV_df.rename(columns={'Date': 'DATE'})
-        OHLCV_df = OHLCV_df.rename(columns={'close': 'CLOSE'})
+        OHLCV_df = OHLCV_df.rename(columns={'Date': 'DATE', 'close': 'CLOSE'})
         OHLCV_df.loc[:, ['OPEN', 'HIGH', 'LOW', 'CLOSE', 'VOLUME']] = OHLCV_df[['OPEN', 'HIGH', 'LOW', 'CLOSE', 'VOLUME']].replace({',': ''}, regex=True).astype(float)
         OHLCV_df.loc[:, ['VOLUME']] = OHLCV_df[['VOLUME']].replace({',': ''}, regex=True).astype(int)
         OHLCV_df.loc[:, 'DATE'] = pd.to_datetime(OHLCV_df['DATE'], format='%d-%b-%Y')
@@ -210,45 +243,45 @@ class NSEPyData():
 
 
     def get_corporate_action_data(self, symbol):
-            url = "https://www.nseindia.com/api/corporates-corporateActions"
+        url = "https://www.nseindia.com/api/corporates-corporateActions"
 
-            # Initialize the final DataFrame
-            nse_df = pd.DataFrame()
+        # Initialize the final DataFrame
+        nse_df = pd.DataFrame()
 
-            # Session to manage cookies automatically
-            with requests.Session() as session:
-                # Send an initial request to the main site to get cookies and headers
-                session.get('https://www.nseindia.com', headers=self.headers)
+        # Session to manage cookies automatically
+        with requests.Session() as session:
+            # Send an initial request to the main site to get cookies and headers
+            session.get('https://www.nseindia.com', headers=self.headers)
 
-                # Update request parameters
-                params = {
-                    "index": "equities",
-                    "symbol": symbol,
-                    "csv": "true"
-                }
+            # Update request parameters
+            params = {
+                "index": "equities",
+                "symbol": symbol,
+                "csv": "true"
+            }
 
-                # Make the actual request to download the CSV file
-                response = session.get(url, headers=self.headers, params=params)
-                
-                # Check if response is empty
-                if not response.content.strip():
+            # Make the actual request to download the CSV file
+            response = session.get(url, headers=self.headers, params=params)
+            
+            # Check if response is empty
+            if not response.content.strip():
+                self.__logger.info("No more data to fetch.")
+
+            # Parse the CSV content and append to the final DataFrame
+            try:
+                df = pd.read_csv(StringIO(response.text))
+                if df.shape[0] != 0:
+                    nse_df = pd.concat([nse_df, df], ignore_index=True)
+                    self.__logger.info(f"Fetched coporate action data for {symbol}.")
+                else:
                     self.__logger.info("No more data to fetch.")
+            except Exception as e:
+                self.__logger.error(f"Error parsing response: {e}")
 
-                # Parse the CSV content and append to the final DataFrame
-                try:
-                    df = pd.read_csv(StringIO(response.text))
-                    if df.shape[0] != 0:
-                        nse_df = pd.concat([nse_df, df], ignore_index=True)
-                        self.__logger.info(f"Fetched coporate action data for {symbol}.")
-                    else:
-                        self.__logger.info("No more data to fetch.")
-                except Exception as e:
-                    self.__logger.error(f"Error parsing response: {e}")
-
-            return nse_df 
+        return nse_df 
 
 
-    def get_OHLCV_data(self, symbol: str, series: str, start: str, end:str=None, 
+    def get_OHLCV_data(self, symbol: str, start: str, end:str=None, 
                        adjust_corp_action:bool=True, timeperiod:str='1D') -> Tuple[pd.DataFrame, pd.DataFrame]:
         """
         Downloads the historical data of a NSE symbol.
@@ -266,59 +299,84 @@ class NSEPyData():
             - The second DataFrame contains corporate action data.
             If no data is found, both DataFrames will be empty.
         """
+        series = nse_ohlcv_df = None
+        symbol = symbol.upper()
+        if symbol in self.equity_df['SYMBOL'].values:
+            series = self.equity_df.loc[self.equity_df['SYMBOL'] == symbol, 'SERIES'].iloc[0]
+        elif symbol in self.sme_df['SYMBOL'].values:
+            series = self.sme_df.loc[self.sme_df['SYMBOL'] == symbol, 'SERIES'].iloc[0]
 
-        fetch_end = datetime.now() if end == None or adjust_corp_action else datetime.strptime(end, '%d-%b-%Y')
-        start = datetime.strptime(start, '%d-%b-%Y')
-        nse_df = self.__download_historical_price_volume(symbol, series, start, fetch_end)
-        nse_ohlcv_df = self.__extractOHLCV(nse_df)
-        
-        if adjust_corp_action:
-            nse_corp_act_df = self.get_corporate_action_data(symbol)
-            nse_ohlcv_df = self.__adjust_for_corp_action(nse_ohlcv_df, nse_corp_act_df)
+        if series is not None:
+            fetch_end = datetime.now() if end == None or adjust_corp_action else datetime.strptime(end, '%d-%b-%Y')
+            start = datetime.strptime(start, '%d-%b-%Y')
+            nse_df = self.__download_historical_price_volume(symbol, series, start, fetch_end)
+            nse_ohlcv_df = self.__extractOHLCV(nse_df)
+            
+            if adjust_corp_action:
+                nse_corp_act_df = self.get_corporate_action_data(symbol)
+                nse_ohlcv_df = self.__adjust_for_corp_action(nse_ohlcv_df, nse_corp_act_df)
 
-        if end is not None:
-            end = datetime.strptime(end, '%d-%b-%Y')
-            nse_ohlcv_df = nse_ohlcv_df[(nse_ohlcv_df['DATE'] <= end)]
+            if end is not None:
+                end = datetime.strptime(end, '%d-%b-%Y')
+                nse_ohlcv_df = nse_ohlcv_df[(nse_ohlcv_df['DATE'] <= end)]
 
-        timeperiod = timeperiod.upper()
-        if timeperiod != '1D':
-            nse_ohlcv_df = self.change_time_period(nse_ohlcv_df, timeperiod)
-        
-        nse_ohlcv_df['DATE'] = pd.to_datetime(nse_ohlcv_df['DATE']).dt.strftime('%d-%b-%Y')
+            timeperiod = timeperiod.upper()
+            if timeperiod != '1D':
+                nse_ohlcv_df = self.change_time_period(nse_ohlcv_df, timeperiod)
+            
+            nse_ohlcv_df['DATE'] = pd.to_datetime(nse_ohlcv_df['DATE']).dt.strftime('%d-%b-%Y')
 
         return nse_ohlcv_df
 
 
+    def get_securities_in_segment(self, segment):
+        segment = segment.upper()
+        host = 'nsearchives.nseindia.com'
+        if segment == 'EQUITY':
+            url = 'https://nsearchives.nseindia.com/content/equities/EQUITY_L.csv'
+        elif segment == 'SME':
+            url = 'https://nsearchives.nseindia.com/emerge/corporates/content/SME_EQUITY_L.csv'
+
+        securities_df = self.__download_csv(url, host=host)
+
+        if segment == 'EQUITY':
+            securities_df = securities_df.rename(columns={'NAME OF COMPANY': 'NAME_OF_COMPANY', ' SERIES': 'SERIES', 
+                                                          ' DATE OF LISTING': 'DATE_OF_LISTING', ' PAID UP VALUE': 'PAID_UP_VALUE',
+                                                          ' MARKET LOT': 'MARKET_LOT', ' ISIN NUMBER': 'ISIN_NUMBER', ' FACE VALUE': 'FACE_VALUE', })
+
+        return securities_df
+
+
     def download_bhav(self, date):
-            """
-            Downloads the BhavCopy file for a given date and returns its content.
+        """
+        Downloads the BhavCopy file for a given date and returns its content.
 
-            Args:
-                date (str): Date in the format "DD-MMM-YYYY".
+        Args:
+            date (str): Date in the format "DD-MMM-YYYY".
 
-            Returns:
-                bytes: Content of the downloaded file.
-            """
-            date_download = datetime.strptime(date, "%d-%b-%Y").strftime("%Y%m%d")
-            url = "https://nsearchives.nseindia.com/content/cm/BhavCopy_NSE_CM_0_0_0_<DATE>_F_0000.csv.zip"
-            url = re.sub('<DATE>', date_download, url)
+        Returns:
+            bytes: Content of the downloaded file.
+        """
+        date_download = datetime.strptime(date, "%d-%b-%Y").strftime("%Y%m%d")
+        url = "https://nsearchives.nseindia.com/content/cm/BhavCopy_NSE_CM_0_0_0_<DATE>_F_0000.csv.zip"
+        url = re.sub('<DATE>', date_download, url)
 
-            with requests.Session() as session:
-                try:
-                    # Send an initial request to the main site to get cookies and headers
-                    session.get('https://www.nseindia.com', headers=self.headers)   
+        with requests.Session() as session:
+            try:
+                # Send an initial request to the main site to get cookies and headers
+                session.get('https://www.nseindia.com', headers=self.headers)   
 
-                    # Send a GET request
-                    self.headers['Host'] = 'nsearchives.nseindia.com'
-                    response = session.get(url, headers=self.headers)
-                    response.raise_for_status()  # Raise exception for HTTP errors
-                    
-                    # Return the content of the response
-                    self.__logger.info(f"File downloaded successfully from URL: {url}")
-                    return response.content
-                except requests.exceptions.RequestException as e:
-                    self.__logger.error(f"Error downloading file: {e}")
-                    raise
+                # Send a GET request
+                self.headers['Host'] = 'nsearchives.nseindia.com'
+                response = session.get(url, headers=self.headers)
+                response.raise_for_status()  # Raise exception for HTTP errors
+                
+                # Return the content of the response
+                self.__logger.info(f"File downloaded successfully from URL: {url}")
+                return response.content
+            except requests.exceptions.RequestException as e:
+                self.__logger.error(f"Error downloading file: {e}")
+                raise
 
 
     def download_index_bhav(self):
@@ -346,4 +404,7 @@ class NSEPyData():
                 self.__logger.error(f"Error downloading file: {e}")
                 raise
 
+
+    def __download_all_corporate_actions(self):
+        url = "https://www.nseindia.com/api/corporates-corporateActions?index=equities&from_date=05-01-2024&to_date=05-12-2024&csv=true"
 
